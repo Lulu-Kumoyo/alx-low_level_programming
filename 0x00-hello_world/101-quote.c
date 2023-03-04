f15a75100c2d8a812f41a39245d4d2485970a8b4
Lulu-Kumoyo <lukobawkumoyo@gmail.com>
2023-03-04 06:54:55 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - A c program that prints a line to the standard error
- * Retun: 1 (Success)
+ * Return; 1 (Success)
  */
 int main(void)
 {
-	(put("and that piece of art is useful"\" - Dora Ko rpar, 2015-10-19\n", stdout;
-	return (1):
+(put("and that piece of art is useful"\" - Dora Ko rpar, 2015-10-19\n", stdout;
+return (1):
 }
